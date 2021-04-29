203449ba9c7c3ae578b2e2de13b8d4259765384b
Christoph Ploiner <christoph.ploiner@energyagency.at>
2021-04-29 09:07:23 +0200

    initial commit (+) added matplot04

---
+++ Lesson12_matplot04.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.normal(100, 15, 200)  # Generierung von zufälligen Zahlen in einer gaußschen Verteilung (Mittelwert, Bereich, Anzahl Werte)
+
+plt.hist(x)
+plt.show()
