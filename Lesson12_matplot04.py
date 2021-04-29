d13589aade01a419f86949160525c5fd7632cc5a
Christoph Ploiner <christoph.ploiner@energyagency.at>
2021-04-29 09:29:07 +0200

    print added

--- Lesson12_matplot04.py
+++ Lesson12_matplot04.py
@@ -4,6 +4,7 @@ import numpy as np
 x = np.random.normal(100, 15, 200)  # Generierung von zufälligen Zahlen in einer gaußschen Verteilung (Mittelwert, Bereich, Anzahl Werte)
 
 print(x)
+print(x)
 
 plt.hist(x)
 plt.show()
